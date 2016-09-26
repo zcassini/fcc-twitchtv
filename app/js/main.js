bde4f78b932d62ab3620dbde222a2947dcb7a730
Zac Cassini <zcassini@gmail.com>
2016-09-26 14:55:49 -0400

    pretty pretty pretty good

--- app/js/main.js
+++ app/js/main.js
@@ -1,33 +1,36 @@
-console.log('Aloha Mr. Hand')
 import $ from 'jquery'
-var searchTerms = 'tooth'
+
+var url = 'https://en.wikipedia.org/w/api.php?action=opensearch&format=json&limit=10&profile=fuzzy&callback=?&search='
+var randomUrl = 'https://en.wikipedia.org/wiki/Special:Random'
+var urls = []
+
 $('document').ready(function () {
-  var wikipedia = 'https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&srsearch=' + searchTerms + '&callback=?'
-  $.getJSON(wikipedia, function (jsonp) {
-    // $("#jsonp-response").html(JSON.stringify(jsonp, null, 2));
-    var results = jsonp['query']['search']
-    $.each(results, function (key, value) {
-      $('body').append('<h3>' + value.title + '</h3>')
-      $('body').append('<p>' + value.snippet + '</p>')
-    })
-    console.log(results)
-    // console.log(jsonp)
-    if (jsonp.status == 'OK') {
-      // Check result 0
-      var result = jsonp.results[0]
-      // look for locality tag and administrative_area_level_1
-     // console.log(jsonp)
-      for (var i = 0; i < jsonp.results; i++) {
-       // console.log(jsonp)
-      }
-      // var url = host_prefix + '/jsonp?callback=?';
-      // $.getJSON(url, function(jsonp) {
-      //   $("#jsonp-response").html(JSON.stringify(jsonp, null, 2));
-      // });
-      // if (state != '') {
-      //   console.log("Hello to you out there in " + city + ", " + state + "!");
-      // }
-      // $('body').append(city + "," + state);
-    }
+  $('#searchbox').keypress(function(e) {
+    if (e.keyCode === 13) search(this.value)
+  })
+  $('#random').click(function() {
+    openWin(randomUrl)
   })
 })
+
+function search (searchTerm) {
+  $('#results').html('')
+  urls = []
+
+  $.getJSON(url + searchTerm, function (jsonp) {
+    for(var i = 0; i < jsonp[1].length; i++) {
+      var title = '<h5 class="title">' + jsonp[1][i] + '</h5>'
+      var summary = '<p class="snippet">' + jsonp[2][i] + '</p>'
+      urls.push(jsonp[3][i])
+
+      $('#results').append('<div class="grid-flex-container panel" id="' + i + '">' + title + '<br />' + summary + '</div>')
+      $(`#${i}`).on('click', function () {
+        openWin(urls[$(this).attr('id')])//, '_blank')
+      })
+    }
+  })
+}
+
+function openWin (link) {
+  window.open(link, '_blank') ? window.focus() : alert('Please allow popups for this website')
+}
